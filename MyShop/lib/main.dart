144220ddfb1398d19d55bea99f0e3255ebc3a4bc
ashish-kumar-7 <ashishkumar307010@gmail.com>
2020-11-15 12:36:57 +0530

    Login Page Added

--- MyShop/lib/main.dart
+++ MyShop/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:MyShop/providers/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -7,9 +8,11 @@ import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import './providers/auth.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,6 +20,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(providers: [
+      ChangeNotifierProvider(
+        builder: (ctx) => Auth(),
+        ),
       ChangeNotifierProvider(
       builder: (ctx) => Products(),
       ),
@@ -34,7 +40,7 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
